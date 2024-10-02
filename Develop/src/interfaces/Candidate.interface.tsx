4c5ae28940009f8bafad0afac7649951cc4306a9
Sabrina-Sawyer <sabrinagtc242@gmail.com>
2024-10-02 17:02:39 -0400

    added candidate card

--- Develop/src/interfaces/Candidate.interface.tsx
+++ Develop/src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,12 @@
 // TODO: Create an interface for the Candidate objects returned by the API
+export interface Candidate {
+    login: string | null;
+    name: string | null;
+    id: number;
+    location: string | null;
+    img: string | null;
+    email: string | null;
+    html_url: string | null;
+    company: string | null;
+    bio: string;
+}
--- Develop/src/pages/CandidateSearch.tsx
+++ Develop/src/pages/CandidateSearch.tsx
@@ -1,7 +1,21 @@
 import { useState, useEffect } from 'react';
 import { searchGithub, searchGithubUser } from '../api/API';
+import { Candidate } from '../interfaces/Candidate.interface';
+import CandidateCard from '../components/CandidateCards';
 
 const CandidateSearch = () => {
+const [currentCandidate, setCurrentCandidate] = useState<Candidate>({
+  login: '',
+  name: '',
+  id: '',
+  location: '',
+  img: '',
+  email: '',
+  html_url: '',
+  company: '',
+  bio: '',
+});
+
   return <h1>CandidateSearch</h1>;
 };
 
